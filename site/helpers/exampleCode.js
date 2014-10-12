ad8de021a1a233ef94bf8a42c058fca7ca1613c4
Tim Jones <tim@timjones.tw>
2014-10-13 01:43:34 +0800

    Rename examples

--- site/helpers/exampleCode.js
+++ site/helpers/exampleCode.js
@@ -4,7 +4,7 @@ var hljs = require("highlight.js");
 
 module.exports.register = function (Handlebars, options, params)  { 
   Handlebars.registerHelper('exampleCode', function (src)  { 
-    var path = "./site/assets/js/example_" + src + ".js";
+    var path = "./site/assets/js/example-" + src + ".js";
     var content = glob.globFiles(path);
     var highlighted = hljs.highlight('js', content).value;
     return new Handlebars.SafeString('<pre class="hljs">' + highlighted + "</pre>");
