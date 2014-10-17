cc7fbdbaf6ed1986c68f340b31a626b765105bbf
Tim Jones <tim@timjones.tw>
2014-10-17 21:48:33 +0800

    Updates

--- assets/js/example-hello-world.js
+++ assets/js/example-hello-world.js
@@ -2,31 +2,49 @@ var canvas = document.getElementById('mainCanvas');
 
 var graphicsDevice = new Zia.GraphicsDevice(canvas);
 
-var program = new Zia.BasicProgram(graphicsDevice);
+var program = new Zia.BasicProgram(graphicsDevice, {
+  lightingEnabled: true
+});
+program.diffuseColor = new Zia.Vector3(0.5, 1, 0.5);
+program.enableDefaultLighting();
+
+var cubePrimitive = Zia.GeometricPrimitive.createCube();
+
+var vertexData = Zia.GeometricPrimitive.mergeVertexData(
+  cubePrimitive.positions, cubePrimitive.normals,
+  cubePrimitive.textureCoordinates);
+var vertices = new Float32Array(vertexData);
 
 var vertexBuffer = new Zia.VertexBuffer(graphicsDevice,
-  Zia.BoxPrimitive.vertexDeclaration,
-  Zia.BoxPrimitive.vertices);
+  new Zia.VertexDeclaration(
+  [
+    new Zia.VertexElement("aVertexPosition", 3, 0),
+    new Zia.VertexElement("aVertexNormal", 3, 3 * 4),
+    new Zia.VertexElement("aTextureCoord", 2, 6 * 4)
+  ]),
+  vertices);
 
 var indexBuffer = new Zia.IndexBuffer(graphicsDevice,
-  Zia.BoxPrimitive.indices);
+  new Uint16Array(cubePrimitive.indices));
 
 var projectionMatrix = new Zia.Matrix4().makePerspective(45,
   graphicsDevice.viewport.aspectRatio, 0.1, 100);
-var viewMatrix = new Zia.Matrix4().makeTranslation(0, 0, -4);
+var viewMatrix = new Zia.Matrix4().makeLookAt(
+  new Zia.Vector3(1, 1, -1.5),
+  new Zia.Vector3(0, 0, 0),
+  new Zia.Vector3(0, 1, 0));
 var modelMatrix = new Zia.Matrix4().identity();
 
-program.view = viewMatrix;
-program.projection = projectionMatrix;
-
 graphicsDevice.setIndexBuffer(indexBuffer);
-graphicsDevice.setVertexBuffers([vertexBuffer]);
+graphicsDevice.setVertexBuffer(vertexBuffer);
 
 graphicsDevice.clear(
   Zia.ClearOptions.ColorBuffer | Zia.ClearOptions.DepthBuffer,
   new Zia.Color4(0, 0, 0, 1), 1);
 
-program.model = modelMatrix;
+program.viewMatrix = viewMatrix;
+program.projectionMatrix = projectionMatrix;
+program.modelMatrix = modelMatrix;
 program.apply();
 
 graphicsDevice.drawIndexedPrimitives(
